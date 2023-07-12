6758490f6a326dc9ab56a644e7e2b7098a87aa3a
Hasibul Habib <hasibulhabib1601@gmail.com>
2023-07-12 17:17:41 +0600

    BFS_intro_1.py file added

---
+++ BFS/BFS_intro_1.py
@@ -0,0 +1,32 @@
+from queue import Queue
+
+def bfs(edges:list[list[int]], n:int) -> None:
+    graph = [ set() for _ in range(n) ]
+    for (u,v) in edges:
+        graph[u].add(v)
+        graph[v].add(u)
+
+    q = Queue()
+    visited = [False] * n
+    level = [0] * n
+    q.put(0)
+    visited[0] = True
+    
+    while not q.empty():
+        cur_node = q.get()
+        print(cur_node, end=" ")
+        for v in graph[cur_node]:
+            if visited[v] == False:
+                q.put(v)
+                visited[v] = True
+                level[v] = level[cur_node] + 1
+                
+    print()
+    print('\n'.join( f'{i}  :  {v}' for i,v in enumerate(level)))
+
+def main():
+    edges = [[0,1],[0,2],[0,9],[1,5],[1,6],[2,4],[4,7],[6,8],[7,3]]
+    bfs(edges, 10)
+
+if __name__ == "__main__":
+    main()
